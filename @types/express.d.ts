687580c093a4dc11d2fee226a73b4eb0dcababdb
ervnjmsdnts <earvinjamesdantes@gmail.com>
2024-11-23 04:13:15 +0800

    yes

--- @types/express.d.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: any; // You can replace `any` with a more specific type for your user if you have one, e.g., `User`
-    }
-  }
-}
--- src/routes/index.ts
+++
@@ -1,5 +0,0 @@
-import { default as facilityRoutes } from './facility-routes';
-import { default as healthProfessionalRoutes } from './health-professional-routes';
-import { default as authRoutes } from './auth-routes';
-
-export { facilityRoutes, healthProfessionalRoutes, authRoutes };
